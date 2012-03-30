6da91209cbe2d55e08e45362fa21aae16c9de461
Trotter Cashion <cashion@gmail.com>
2012-03-30 13:18:36 -0700

    Pass logfile to component definition

--- recipes/gateway.rb
+++ recipes/gateway.rb
@@ -2,4 +2,5 @@ cloudfoundry_component "mysql_gateway" do
   install_path File.join(node.cloudfoundry_common.vcap.install_path, "services", "mysql")
   bin_file     File.join(node.cloudfoundry_common.vcap.install_path, "bin", "services", "mysql_gateway")
   pid_file     node.cloudfoundry_mysql_service.gateway.pid_file
+  log_file     node.cloudfoundry_mysql_service.gateway.log_file
 end
